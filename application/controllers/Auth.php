13129975619d33678cb86fdb84885ffec861ff5a
adprm <aditiyaprmn00@gmail.com>
2020-07-29 11:13:30 +0700

    make method registration

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -8,5 +8,11 @@ class Auth extends CI_Controller {
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
     }
+    
+    public function registration() {
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/registration');
+        $this->load->view('templates/auth_footer');
+    }
 
 }
