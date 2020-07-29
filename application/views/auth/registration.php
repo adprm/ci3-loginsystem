4648e8b8a155f73dce9b2d23b52a2e2ffc6b710b
adprm <aditiyaprmn00@gmail.com>
2020-07-29 13:46:34 +0700

    set value and form error

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -11,10 +11,14 @@
               </div>
               <form class="user" method="post" action="<?= base_url('auth/registration') ?>">
                 <div class="form-group">
-                  <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full name">
+                  <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full name" 
+                  value="<?= set_value('name'); ?>">
+                  <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
                 </div>
                 <div class="form-group">
-                  <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address">
+                  <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address"
+                  value="<?= set_value('email'); ?>">
+                  <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
