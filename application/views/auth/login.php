724c98371be22affecd1837b61211edc7fbf6cc5
adprm <aditiyaprmn00@gmail.com>
2020-07-29 10:55:22 +0700

    modif view login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -27,14 +27,13 @@
     <!-- Outer Row -->
     <div class="row justify-content-center">
 
-      <div class="col-xl-10 col-lg-12 col-md-9">
+      <div class="col-lg-7">
 
         <div class="card o-hidden border-0 shadow-lg my-5">
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-              <div class="col-lg-6">
+              <div class="col-lg">
                 <div class="p-5">
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
