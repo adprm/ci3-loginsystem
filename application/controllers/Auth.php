0a4ac9cf07ece6ba7e41b3a8a6348bd32c523082
adprm <aditiyaprmn00@gmail.com>
2020-07-29 15:23:22 +0700

    make method _login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -9,10 +9,34 @@ class Auth extends CI_Controller {
     }
 
     public function index() {
-        $data['title'] = 'Login page';
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/login');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Login page';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        } else {
+            // validation success
+            $this->_login();
+        }
+
+    }
+
+    private function _login() {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+        if ($user) {
+            // jika email ada
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            Email is not registered</div>');
+            redirect('auth');
+        }
     }
     
     public function registration() {
