cf564703aa799b8d6580b2ced034c67ba7b50fe4
adprm <aditiyaprmn00@gmail.com>
2020-07-29 15:00:43 +0700

    set true in data name and email

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -33,8 +33,8 @@ class Auth extends CI_Controller {
             $this->load->view('templates/auth_footer');
         } else {
             $data = [
-                'name' => $this->input->post('name'),
-                'email' => $this->input->post('email'),
+                'name' => $this->input->post('name', true),
+                'email' => $this->input->post('email', true),
                 'image' => 'default.jpg',
                 'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
                 'role_id' => 2,
