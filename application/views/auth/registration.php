a8109b6cdcf849529fca5b2cf34b5b0c959fd42e
adprm <aditiyaprmn00@gmail.com>
2020-07-29 14:04:08 +0700

    form error

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -23,6 +23,7 @@
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
                     <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password">
+                    <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
                   </div>
                   <div class="col-sm-6">
                     <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Repeat Password">
