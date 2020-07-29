e7d92f040ed91bbe5f24579af10c7148bb3a7d98
adprm <aditiyaprmn00@gmail.com>
2020-07-29 14:30:07 +0700

    set session flashdata

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -9,14 +9,17 @@ class Auth extends CI_Controller {
     }
 
     public function index() {
-        $this->load->view('templates/auth_header');
+        $data['title'] = 'Login page';
+        $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
     }
     
     public function registration() {
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'This email has already registered!'
+        ]);
         $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
             'matches' => 'Password dont match!',
             'min_length' => 'password to short'
@@ -36,11 +39,13 @@ class Auth extends CI_Controller {
                 'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
                 'role_id' => 2,
                 'is_active' => 1,
-                'date_created' = time()
+                'date_created' => time()
             ];
 
             $this->db->insert('user', $data);
-
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Congratulation! your accounyt has been created. Please Login</div>');
+            redirect('auth');
         }
     }
 
