6b4e36000ba8454cca6cb1fe9a86ec7152245394
adprm <aditiyaprmn00@gmail.com>
2020-07-29 15:52:34 +0700

    make method index

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,7 +5,7 @@ class User extends CI_Controller {
 
     public function index() {
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-        echo "selamat datang " . $data['user']['name'];
+        echo "Selamat datang " . $data['user']['name'];
     }
-
+    
 }
