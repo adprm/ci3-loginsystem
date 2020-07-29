22c400ce85b9f2319b56e2c141ae0fcfe07e5d5c
adprm <aditiyaprmn00@gmail.com>
2020-07-29 11:01:38 +0700

    load template

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -4,7 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Auth extends CI_Controller {
 
     public function index() {
+        $this->load->view('templates/auth_header');
         $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
     }
 
 }
