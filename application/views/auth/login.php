f59f731665e9b4cf0c9ac4b844363361c032e2c2
adprm <aditiyaprmn00@gmail.com>
2020-07-29 15:23:47 +0700

    set value

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -15,12 +15,15 @@
                     <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
                   </div>
                   <?= $this->session->flashdata('message'); ?>
-                  <form class="user">
+                  <form class="user" method="post" action="<?= base_url('auth'); ?>">
                     <div class="form-group">
-                      <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address...">
+                      <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..."
+                      value="<?= set_value('email'); ?>">
+                      <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <div class="form-group">
                       <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                      <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <button type="submit" class="btn btn-primary btn-user btn-block">
                       Login
