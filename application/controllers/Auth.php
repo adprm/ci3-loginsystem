c3b9d2b7527042fa2efff4f235dc6376a855d7f1
adprm <aditiyaprmn00@gmail.com>
2020-07-29 13:27:07 +0700

    load library form_validation

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,6 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
 
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
     public function index() {
         $this->load->view('templates/auth_header');
         $this->load->view('auth/login');
@@ -10,10 +15,14 @@ class Auth extends CI_Controller {
     }
     
     public function registration() {
-        $data['title'] = 'User Registration';
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/registration');
-        $this->load->view('templates/auth_footer');
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'User Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            echo 'data berhasil ditambahkan';
+        }
     }
 
 }
