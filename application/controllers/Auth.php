e7deea9d99a159774df3fbbc897867ecc0910830
adprm <aditiyaprmn00@gmail.com>
2020-07-29 10:40:44 +0700

    load view

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,4 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
 
+    public function index() {
+        $this->load->view('auth/login');
+    }
+
 }
