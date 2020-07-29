20a272cbb35daef7fbda150a4a49afa6b6defad8
adprm <aditiyaprmn00@gmail.com>
2020-07-29 11:26:09 +0700

    base url create account

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -30,7 +30,7 @@
                     <a class="small" href="forgot-password.html">Forgot Password?</a>
                   </div>
                   <div class="text-center">
-                    <a class="small" href="register.html">Create an Account!</a>
+                    <a class="small" href="<?= base_url('auth/registration') ?>">Create an Account!</a>
                   </div>
                 </div>
               </div>
