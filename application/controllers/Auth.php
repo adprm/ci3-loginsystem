b832f1064c24e339f66e90309a37e0a8d2126f23
adprm <aditiyaprmn00@gmail.com>
2020-07-29 15:56:36 +0700

    make method logout

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -93,4 +93,13 @@ class Auth extends CI_Controller {
         }
     }
 
+    public function logout() {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+        You have been logged out!</div>');
+        redirect('auth');
+    }
+
 }
