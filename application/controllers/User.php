d85a2f4183dc175dd31192af9ce84ad4965616f1
adprm <aditiyaprmn00@gmail.com>
2020-07-30 09:16:36 +0700

    make load view index

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,7 +5,8 @@ class User extends CI_Controller {
 
     public function index() {
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-        echo "Selamat datang " . $data['user']['name'];
+        
+        $this->load->view('user/index');
     }
     
 }
