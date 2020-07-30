8d3f161a1d276b70a9e859fa67eda70ea3562364
adprm <aditiyaprmn00@gmail.com>
2020-07-30 10:04:37 +0700

    add data title

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -4,9 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class User extends CI_Controller {
 
     public function index() {
+        $data['title'] = 'My Profile';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         
-        $this->load->view('user/index');
+        $this->load->view('user/index', $data);
     }
     
 }
