bea8b4bbf0d3496d92bd7ccc500a981a8556f032
adprm <aditiyaprmn00@gmail.com>
2020-07-31 13:19:38 +0700

    create Menu management controller

---
+++ application/controllers/Menu.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Menu extends CI_Controller {
+
+    public function index() {
+        $data['title'] = 'Dashboard';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('admin/index', $data);
+        $this->load->view('templates/footer');
+    }
+}
