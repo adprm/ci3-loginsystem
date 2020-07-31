57e3a1fa180f91fe915965567c21ac4767cd4cf3
adprm <aditiyaprmn00@gmail.com>
2020-07-31 13:20:25 +0700

    created title menu management

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Menu extends CI_Controller {
 
     public function index() {
-        $data['title'] = 'Dashboard';
+        $data['title'] = 'Menu Management';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         
         $this->load->view('templates/header', $data);
