1f65cd7d9c9dcce148a4a670b4441334272e3b4c
adprm <aditiyaprmn00@gmail.com>
2020-08-01 10:47:11 +0700

    create method add

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -3,28 +3,40 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Menu extends CI_Controller {
 
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('menu_model');
+    }
+
     public function index() {
         $data['title'] = 'Menu Management';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
         $data['menu'] = $this->db->get('user_menu')->result_array();
 
-        $this->form_validation->set_rules('menu', 'Menu', 'required');
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('menu/index', $data);
+        $this->load->view('templates/footer');
 
-        if ($this->form_validation->run() == false) {
-            $this->load->view('templates/header', $data);
-            $this->load->view('templates/sidebar', $data);
-            $this->load->view('templates/topbar', $data);
-            $this->load->view('menu/index', $data);
-            $this->load->view('templates/footer');
-        } else {
-            $this->db->insert('user_menu', ['menu' => $this->input->post('menu')]);
-
-            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
-            New menu added!</div>');
-                    
-            redirect('menu');
-        }
-        
     }
+
+    public function add() {
+        $menu = $this->menu_model;
+
+        $validation = $this->form_validation->set_rules('menu', 'Menu', 'required');
+
+        if ($validation->run() == false) {
+            $this->session->set_flashdata('message_error', '<div class="alert alert-danger" role="alert">
+            New menu dont added!</div>');
+        } else {
+            $menu->save();
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            New menu added!!</div>');
+        }
+
+        redirect('menu');
+    }
+
 }
