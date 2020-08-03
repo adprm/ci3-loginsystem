52c7d62d952c7a9c8056deacb26e63594991d1da
adprm <aditiyaprmn00@gmail.com>
2020-08-03 15:32:59 +0700

    query insert new sub menu

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -88,7 +88,7 @@ class Menu extends CI_Controller {
         $data['menu'] = $this->db->get('user_menu')->result_array();
 
         $validation = $this->form_validation->set_rules('title', 'Title', 'required');
-        $validation = $this->form_validation->set_rules('menu', 'Menu', 'required');
+        $validation = $this->form_validation->set_rules('menu_id', 'Menu_id', 'required');
         $validation = $this->form_validation->set_rules('url', 'Url', 'required');
         $validation = $this->form_validation->set_rules('icon', 'Icon', 'required');
 
@@ -98,6 +98,18 @@ class Menu extends CI_Controller {
             $this->load->view('templates/topbar', $data);
             $this->load->view('menu/submenu', $data);
             $this->load->view('templates/footer');
+        } else {
+            $data = [
+                'title' => $this->input->post('title'),
+                'menu_id' => $this->input->post('menu_id'),
+                'url' => $this->input->post('url'),
+                'icon' => $this->input->post('icon'),
+                'is_active' => $this->input->post('is_active')
+            ];
+            $this->db->insert('user_sub_menu', $data);
+            $this->session->set_flashdata('message_add_new_submenu', '<div class="alert alert-success" role="alert">
+            New sub menu added!</div>');
+            redirect('menu/submenu');
         }
 
     }
