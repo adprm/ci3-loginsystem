88ff19972443a3f83556aec4f584a22d4f53e428
adprm <aditiyaprmn00@gmail.com>
2020-08-03 12:45:05 +0700

    make function get sub menu

--- application/models/Menu_model.php
+++ application/models/Menu_model.php
@@ -3,6 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Menu_model extends CI_Model {
 
+    public function getSubMenu() {
+        $query =    "SELECT `user_sub_menu`.*, `user_menu`.`menu`
+                    FROM `user_sub_menu` JOIN `user_menu`
+                    ON `user_sub_menu`.`menu_id` = `user_menu`.`id` ";
+                    
+        return $this->db->query($query)->result_array();
+    }
+
     private $_table = "user_menu";
 
     public $id;
