b80e2da4c9348c7370a5b8e25524ec430711632d
adprm <aditiyaprmn00@gmail.com>
2020-08-03 12:20:48 +0700

    modified view sub menu

--- application/views/menu/submenu.php
+++ application/views/menu/submenu.php
@@ -9,20 +9,12 @@ function deleteConfirm(url){
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
     <!-- table row -->
     <div class="row">
-        <div class="col-lg-6">
-            <!-- alert error add data -->
-            <?= $this->session->flashdata('message_error'); ?>
-            <!-- alert success add data -->
-            <?= $this->session->flashdata('message'); ?>
-            <!-- alert delete data -->
-            <?= $this->session->flashdata('message_delete'); ?>
-            <!-- alert succesc edit data -->
-            <?= $this->session->flashdata('message_edited_success'); ?>
-            <!-- btn add new menu -->
+        <div class="col-lg">
+            <!-- btn add new submenu -->
             <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newSubMenuModal">Add New Submenu</a>
             <!-- table -->
             <table class="table table-hover">
@@ -43,13 +35,13 @@ function deleteConfirm(url){
                     <tr>
                         <th scope="row"><?= $i ?></th>
                         <td><?= $sm['title']; ?></td>
-                        <td><?= $sm['menu']; ?></td>
+                        <td><?= $sm['menu_id']; ?></td>
                         <td><?= $sm['url']; ?></td>
                         <td><?= $sm['icon']; ?></td>
-                        <td><?= $sm['active']; ?></td>
+                        <td><?= $sm['is_active']; ?></td>
                         <td>
-                            <a class="badge badge-success" href="<?= site_url('menu/edit/'.$m['id']); ?>">edit</a>
-                            <a class="badge badge-danger" href="#!" onclick="deleteConfirm('<?php echo site_url('menu/delete/'.$m['id']) ?>')">delete</a>
+                            <a class="badge badge-success" href="#">edit</a>
+                            <a class="badge badge-danger" href="#">delete</a>
                         </td>
                     </tr>
                     <?php $i++; ?>
@@ -91,34 +83,7 @@ function deleteConfirm(url){
   </div>
 </div>
 
-<!-- modal edit menu -->
-<div class="modal fade" id="editMenuModal" tabindex="-1" role="dialog" aria-labelledby="editMenuModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="editMenuModalLabel">Edit Menu</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <!-- form -->
-      <form action="<?= site_url('menu/edit') . $m['id']; ?>" method="post">
-        <div class="modal-body">
-            <div class="form-group">
-                <input type="text" class="form-control" id="id" name="menu" placeholder="Menu name">
-            </div>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary">Update</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-
 <!-- modal delete -->
-<!-- Logout Delete Confirmation-->
 <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
