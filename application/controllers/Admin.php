da649972a5e6689652b040af308b191835fd07fc
adprm <aditiyaprmn00@gmail.com>
2020-08-05 06:38:32 +0700

    url security

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,6 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
 
+    // tidak menerima user yang belum login
+    public function __construct() {
+        parent::__construct();
+        if (!$this->session->userdata('email')) {
+            redirect('auth');
+        }
+    }
+
     public function index() {
         $data['title'] = 'Dashboard';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
