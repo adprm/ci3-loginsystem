50b2e80ddd19578a3ba680c8386992586b79baa0
adprm <aditiyaprmn00@gmail.com>
2020-08-04 11:42:58 +0700

    make function update submenu

--- application/models/Menu_model.php
+++ application/models/Menu_model.php
@@ -65,4 +65,16 @@ class Menu_model extends CI_Model {
         return $this->db->insert($this->_tableSubmenu, $this);
     }
 
+    public function updateSubmenu() {
+        $post = $this->input->post();
+        $this->id = $post['id'];
+        $this->menu_id = $post['menu_id'];
+        $this->title = $post['title'];
+        $this->url = $post['url'];
+        $this->icon = $post['icon'];
+        $this->is_active = $post['is_active'];
+
+        return $this->db->update($this->_tableSubmenu, $this, array('id' => $post['id']));
+    }
+
 }
