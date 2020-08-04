a810c65014c07e420027b9ea1949e78894c2b707
adprm <aditiyaprmn00@gmail.com>
2020-08-04 11:52:37 +0700

    refactor code

--- application/models/Menu_model.php
+++ application/models/Menu_model.php
@@ -40,45 +40,4 @@ class Menu_model extends CI_Model {
         return $this->db->delete($this->_tableMenu, array("id" => $id));
     }
 
-    // dbsubmenu
-    private $_tableSubmenu;
-
-    public $id;
-    public $menu_id;
-    public $title;
-    public $url;
-    public $icon;
-    public $is_active;
-
-    public function getByIdSubmenu($id) {
-        return $this->db->get_where($this->_tableSubmenu, ['id' => $id])->row();
-    }
-
-    public function saveSubmenu() {
-        $post = $this->input->post();
-        $this->menu_id = $post['menu_id'];
-        $this->title = $post['title'];
-        $this->url = $post-['url'];
-        $this->icon = $post['icon'];
-        $this->is_active = $post['is_active'];
-
-        return $this->db->insert($this->_tableSubmenu, $this);
-    }
-
-    public function updateSubmenu() {
-        $post = $this->input->post();
-        $this->id = $post['id'];
-        $this->menu_id = $post['menu_id'];
-        $this->title = $post['title'];
-        $this->url = $post['url'];
-        $this->icon = $post['icon'];
-        $this->is_active = $post['is_active'];
-
-        return $this->db->update($this->_tableSubmenu, $this, array('id' => $post['id']));
-    }
-
-    public function deleteSubmenu($id) {
-        return $this->db->delete($this->_tableSubmenu, array('id' => $id));
-    }
-
 }
