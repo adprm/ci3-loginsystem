9dd6c7636bc260345edd4695b405106dc56752e7
adprm <aditiyaprmn00@gmail.com>
2020-08-05 06:38:53 +0700

    url security

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,6 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
 
+    // tidak menerima user yang belum login
+    public function __construct() {
+        parent::__construct();
+        if (!$this->session->userdata('email')) {
+            redirect('auth');
+        }
+    }
+
     public function index() {
         $data['title'] = 'My Profile';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
