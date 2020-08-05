bf98e1b13eda8969efbb4ca9aab3ff552d1e6b19
adprm <aditiyaprmn00@gmail.com>
2020-08-06 04:12:31 +0700

    make function check_access

--- application/helpers/adprm_helper.php
+++ application/helpers/adprm_helper.php
@@ -23,3 +23,11 @@ function is_logged_in() {
         }
     }
 }
+
+function check_access($role_id, $menu_id) {
+    $ci = get_instance();
+
+    $ci->db->where('role_id', $role_id);
+    $ci->db->where('menu_id', $menu_id);
+    $ci->db->get('user_access_menu');
+}
