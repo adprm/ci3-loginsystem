d974925193933d04700a456cae65d72bbe2d0e41
adprm <aditiyaprmn00@gmail.com>
2020-08-06 04:15:32 +0700

    return checked

--- application/helpers/adprm_helper.php
+++ application/helpers/adprm_helper.php
@@ -29,5 +29,9 @@ function check_access($role_id, $menu_id) {
 
     $ci->db->where('role_id', $role_id);
     $ci->db->where('menu_id', $menu_id);
-    $ci->db->get('user_access_menu');
+    $result = $ci->db->get('user_access_menu');
+
+    if ($result->num_rows() > 0) {
+        return "checked='checked'";
+    }
 }
