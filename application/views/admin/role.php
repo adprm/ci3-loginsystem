210e97ca47086f769fc30c98e5cadb1d0cae83a9
adprm <aditiyaprmn00@gmail.com>
2020-08-06 03:38:40 +0700

    view role

--- application/views/admin/role.php
+++ application/views/admin/role.php
@@ -25,25 +25,26 @@ function deleteConfirm(url){
             <!-- alert error edit data -->
             <?= $this->session->flashdata('message_error_editmenu'); ?>
             <!-- btn add new menu -->
-            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newRoleModal">Add New Menu</a>
+            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newRoleModal">Add New Role</a>
             <!-- table -->
             <table class="table table-hover">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Menu</th>
+                        <th scope="col">Role</th>
                         <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php  $i = 1; ?>
-                    <?php foreach($menu as $m) : ?>
+                    <?php foreach($role as $r) : ?>
                     <tr>
                         <th scope="row"><?= $i ?></th>
-                        <td><?= $m['menu']; ?></td>
+                        <td><?= $r['role']; ?></td>
                         <td>
-                            <a class="badge badge-success" href="<?= site_url('menu/editMenu/'.$m['id']); ?>">edit</a>
-                            <a class="badge badge-danger" href="#!" onclick="deleteConfirm('<?php echo site_url('menu/deleteMenu/'.$m['id']) ?>')">delete</a>
+                            <a class="badge badge-success" href="<?= site_url('menu/editMenu/'.$r['id']); ?>">edit</a>
+                            <a class="badge badge-danger" href="#!" 
+                            onclick="deleteConfirm('<?php echo site_url('menu/deleteMenu/'.$r['id']) ?>')">delete</a>
                         </td>
                     </tr>
                     <?php $i++; ?>
@@ -60,11 +61,11 @@ function deleteConfirm(url){
 <!-- End of Main Content -->
 
 <!-- Modal add new menu-->
-<div class="modal fade" id="newMenuModal" tabindex="-1" role="dialog" aria-labelledby="newMenuModalLabel" aria-hidden="true">
+<div class="modal fade" id="newRoleModal" tabindex="-1" role="dialog" aria-labelledby="newRoleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="newMenuModalLabel">Add New Menu</h5>
+        <h5 class="modal-title" id="newRoleModalLabel">Add New Role</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
