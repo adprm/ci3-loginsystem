2de784e99f927b8dd317ec3dd6774d8c60569c07
adprm <aditiyaprmn00@gmail.com>
2020-08-05 07:06:57 +0700

    user access menu

--- application/helpers/adprm_helper.php
+++ application/helpers/adprm_helper.php
@@ -11,6 +11,15 @@ function is_logged_in() {
         $menu = $ci->uri->segment(1);
 
         $queryMenu = $ci->db->get_where('user_menu', ['menu' => $menu])->row_array();
-        $mneu_id = $queryMenu['id'];
+        $menu_id = $queryMenu['id'];
+
+        $userAccess = $ci->db->get_where('user_access_menu', [
+            'role_id' => $role_id;
+            'menu_id' => $menu_id;
+        ]);
+
+        if ($userAccess->num_rows() < 1) {
+            redirect('auth/blocked');
+        }
     }
 }
