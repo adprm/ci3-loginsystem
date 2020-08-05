33f0eb52cda4011dbb707f1cbad4da57436604eb
adprm <aditiyaprmn00@gmail.com>
2020-08-05 07:02:20 +0700

    query menu

--- application/helpers/adprm_helper.php
+++ application/helpers/adprm_helper.php
@@ -10,6 +10,7 @@ function is_logged_in() {
         $role_id = $ci->session->userdata('role_id');
         $menu = $ci->uri->segment(1);
 
-        
+        $queryMenu = $ci->db->get_where('user_menu', ['menu' => $menu])->row_array();
+        $mneu_id = $queryMenu['id'];
     }
 }
