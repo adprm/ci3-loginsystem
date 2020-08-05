cb0a8b4fe5b4039cf9e867b858628f9f9d2623d5
adprm <aditiyaprmn00@gmail.com>
2020-08-06 03:59:20 +0700

    view role access

---
+++ application/views/admin/role-access.php
@@ -0,0 +1,50 @@
+<script>
+function deleteConfirm(url){
+	$('#btn-delete').attr('href', url);
+	$('#deleteModal').modal();
+}
+</script>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
+
+    <!-- table row -->
+    <div class="row">
+        <div class="col-lg-6">
+            <!-- flashdata roleaccess -->
+            <?= $this->session->flashdata('message'); ?>
+
+            <!-- table -->
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Menu</th>
+                        <th scope="col">Access</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php  $i = 1; ?>
+                    <?php foreach($menu as $m) : ?>
+                    <tr>
+                        <th scope="row"><?= $i ?></th>
+                        <td><?= $m['menu']; ?></td>
+                        <td>
+                            
+                        </td>
+                    </tr>
+                    <?php $i++; ?>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
