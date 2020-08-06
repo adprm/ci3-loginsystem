f52a3b4ff84ce00150c3b95882cd6f66e4bfbe95
adprm <aditiyaprmn00@gmail.com>
2020-08-07 03:03:25 +0700

    make method edit profile

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -19,5 +19,16 @@ class User extends CI_Controller {
         $this->load->view('user/index', $data);
         $this->load->view('templates/footer');
     }
+
+    public function edit() {
+        $data['title'] = 'Edit Profile';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/index', $data);
+        $this->load->view('templates/footer');
+    }
     
 }
