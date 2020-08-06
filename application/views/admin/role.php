073bd4ea5bc0bb8a8f57322be1bbe41a8ebddd5f
adprm <aditiyaprmn00@gmail.com>
2020-08-07 02:54:35 +0700

    deleted code not used

--- application/views/admin/role.php
+++ application/views/admin/role.php
@@ -1,10 +1,3 @@
-<script>
-function deleteConfirm(url){
-	$('#btn-delete').attr('href', url);
-	$('#deleteModal').modal();
-}
-</script>
-
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
@@ -14,18 +7,6 @@ function deleteConfirm(url){
     <!-- table row -->
     <div class="row">
         <div class="col-lg-6">
-            <!-- alert error add data -->
-            <?= $this->session->flashdata('message_error_addmenu'); ?>
-            <!-- alert success add data -->
-            <?= $this->session->flashdata('message_success_addmenu'); ?>
-            <!-- alert delete data -->
-            <?= $this->session->flashdata('message_delete_menu'); ?>
-            <!-- alert success edit data -->
-            <?= $this->session->flashdata('message_success_editmenu'); ?>
-            <!-- alert error edit data -->
-            <?= $this->session->flashdata('message_error_editmenu'); ?>
-            <!-- btn add new menu -->
-            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newRoleModal">Add New Role</a>
             <!-- table -->
             <table class="table table-hover">
                 <thead>
@@ -43,9 +24,6 @@ function deleteConfirm(url){
                         <td><?= $r['role']; ?></td>
                         <td>
                             <a class="badge badge-warning" href="<?= base_url('admin/roleaccess/') . $r['id']; ?>">access</a>
-                            <a class="badge badge-success" href="<?= site_url('menu/editMenu/'.$r['id']); ?>">edit</a>
-                            <a class="badge badge-danger" href="#!" 
-                            onclick="deleteConfirm('<?php echo site_url('menu/deleteMenu/'.$r['id']) ?>')">delete</a>
                         </td>
                     </tr>
                     <?php $i++; ?>
@@ -60,48 +38,3 @@ function deleteConfirm(url){
 
 </div>
 <!-- End of Main Content -->
-
-<!-- Modal add new menu-->
-<div class="modal fade" id="newRoleModal" tabindex="-1" role="dialog" aria-labelledby="newRoleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="newRoleModalLabel">Add New Role</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <!-- form -->
-      <form action="<?= site_url('admin/role'); ?>" method="post">
-        <div class="modal-body">
-            <div class="form-group">
-                <input type="text" class="form-control" id="role" name="role" placeholder="Role name">
-            </div>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary">Add</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-
-<!-- modal delete -->
-<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Are you sure?</h5>
-        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">×</span>
-        </button>
-      </div>
-      <div class="modal-body">Deleted data will not be restored.</div>
-      <div class="modal-footer">
-        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-        <a id="btn-delete" class="btn btn-danger" href="#">Delete</a>
-      </div>
-    </div>
-  </div>
-</div>
