80dbe2b51defd9b7827033090ad0127ceaf502bb
adprm <aditiyaprmn00@gmail.com>
2020-08-07 03:55:50 +0700

    make form edit profile

--- application/views/user/Edit.php
+++ application/views/user/Edit.php
@@ -3,6 +3,43 @@
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
+
+    <div class="row">
+        <div class="col-lg-8">
+            <?= form_open_multipart('user/edit'); ?>
+                <div class="form-group row">
+                    <label for="email" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="email" name="email">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="name" class="col-sm-2 col-form-label">Full name</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="name" name="name">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-2">
+                        Picture
+                    </div>
+                    <div class="col-sm-10">
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <img src="" class="img-thumbnail">
+                            </div>
+                            <div class="col-sm-9">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="image" name="image">
+                                    <label class="custom-file-label" for="image">Choose file</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
     
 </div>
 <!-- /.container-fluid -->
