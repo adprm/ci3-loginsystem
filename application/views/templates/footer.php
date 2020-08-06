b289cb77315f18a6e293b4ce181a069699d01d3f
adprm <aditiyaprmn00@gmail.com>
2020-08-07 04:11:04 +0700

    custom file input for image

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -49,6 +49,12 @@
 <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
 
 <script>
+
+$('.custom-file-input').on('change', function() {
+    let fileName = $(this).val().split('\\').pop();
+    $(this).next('.custom-file-label').addClass("selected").html(fileName);
+});
+
 $('.form-check-input').on('click', function() {
     const menuId = $(this).data('menu');
     const roleId = $(this).data('role');
