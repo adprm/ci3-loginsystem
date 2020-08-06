a1465002dc757835f0c06f20bb4200fbb8b33ccd
adprm <aditiyaprmn00@gmail.com>
2020-08-07 03:04:07 +0700

    load view edit profile

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -27,7 +27,7 @@ class User extends CI_Controller {
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('templates/topbar', $data);
-        $this->load->view('user/index', $data);
+        $this->load->view('user/edit', $data);
         $this->load->view('templates/footer');
     }
     
