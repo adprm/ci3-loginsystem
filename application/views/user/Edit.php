556a0eb63144c3050b4b50e6203880ccc54c6ac2
adprm <aditiyaprmn00@gmail.com>
2020-08-07 04:23:39 +0700

    set alert success edited

--- application/views/user/Edit.php
+++ application/views/user/Edit.php
@@ -5,6 +5,12 @@
     <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
 
     <div class="row">
+        <div class="col-lg-6">
+            <?= $this->session->flashdata('message_success_editprofile'); ?>
+        </div>
+    </div>
+
+    <div class="row">
         <div class="col-lg-8">
             <?= form_open_multipart('user/edit'); ?>
                 <div class="form-group row">
