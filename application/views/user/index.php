67a5adef440a4567ecddbf2f66129f707e872b8f
adprm <aditiyaprmn00@gmail.com>
2020-08-07 04:31:59 +0700

    set alert message

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -4,6 +4,12 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
 
+    <div class="row">
+        <div class="col-lg-6">
+            <?= $this->session->flashdata('message_success_editprofile'); ?>
+        </div>
+    </div>
+
     <!-- card profile -->
     <div class="card mb-3" style="max-width: 540px;">
         <div class="row no-gutters">
