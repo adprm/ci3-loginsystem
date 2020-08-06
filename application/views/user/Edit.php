be732b02f19b0e252acadb8a15eb1164905a0036
adprm <aditiyaprmn00@gmail.com>
2020-08-07 04:16:25 +0700

    set form error

--- application/views/user/Edit.php
+++ application/views/user/Edit.php
@@ -10,13 +10,14 @@
                 <div class="form-group row">
                     <label for="email" class="col-sm-2 col-form-label">Email</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="email" name="email">
+                        <input type="text" class="form-control" id="email" name="email" value="<?= $user['email']; ?>" readonly>
                     </div>
                 </div>
                 <div class="form-group row">
                     <label for="name" class="col-sm-2 col-form-label">Full name</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="name" name="name">
+                        <input type="text" class="form-control" id="name" name="name" value="<?= $user['name']; ?>">
+                        <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                 </div>
                 <div class="form-group row">
@@ -26,7 +27,7 @@
                     <div class="col-sm-10">
                         <div class="row">
                             <div class="col-sm-3">
-                                <img src="" class="img-thumbnail">
+                                <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="img-thumbnail">
                             </div>
                             <div class="col-sm-9">
                                 <div class="custom-file">
