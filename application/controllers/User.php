8db04e09e53f294e1a181a6d4114637bca75c767
adprm <aditiyaprmn00@gmail.com>
2020-08-07 04:13:21 +0700

    add form validation

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -23,12 +23,15 @@ class User extends CI_Controller {
     public function edit() {
         $data['title'] = 'Edit Profile';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('user/edit', $data);
+            $this->load->view('templates/footer');
+        }
         
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar', $data);
-        $this->load->view('templates/topbar', $data);
-        $this->load->view('user/edit', $data);
-        $this->load->view('templates/footer');
     }
     
 }
