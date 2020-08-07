78209bad445391bfad568d40d3cc7ff3570a84cd
adprm <aditiyaprmn00@gmail.com>
2020-08-07 09:49:33 +0700

    set alert message

--- application/views/user/changepassword.php
+++ application/views/user/changepassword.php
@@ -6,6 +6,12 @@
 
     <div class="row">
         <div class="col-lg-6">
+            <!-- input error current pasword -->
+            <?= $this->session->flashdata('message_error_inputcurrentpassword'); ?>
+            <!-- input error new password -->
+            <?= $this->session->flashdata('message_error_inputnewpassword'); ?>
+            <!-- input success change password -->
+            <?= $this->session->flashdata('message_success_changepassword'); ?>
             <form action="<?= base_url('user/changepassword'); ?>" method="post">
                 <div class="form-group">
                     <label for="current_password">Current Password</label>
