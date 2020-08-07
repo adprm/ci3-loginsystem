346033f5974553cdfc8048d668e39e1ac3841ef3
adprm <aditiyaprmn00@gmail.com>
2020-08-07 09:12:57 +0700

    modified view change password

--- application/views/user/changepassword.php
+++ application/views/user/changepassword.php
@@ -3,6 +3,14 @@
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
+
+    <div class="row">
+        <div class="col-lg-6">
+            <form action="<?= base_url('user/changepassword'); ?>" method="post">
+                
+            </form>
+        </div>
+    </div>
     
 </div>
 <!-- /.container-fluid -->
