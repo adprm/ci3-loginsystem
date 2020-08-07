11bb8f930cecce8e095f839b59370aa152dca8d8
adprm <aditiyaprmn00@gmail.com>
2020-08-07 09:30:15 +0700

    set form error validation

--- application/views/user/changepassword.php
+++ application/views/user/changepassword.php
@@ -10,10 +10,12 @@
                 <div class="form-group">
                     <label for="current_password">Current Password</label>
                     <input type="password" class="form-control" id="current_password" name="current_password">
+                    <?= form_error('current_password', '<small class="text-danger pl-3">', '</small>'); ?>
                 </div>
                 <div class="form-group">
                     <label for="new_password1">New Password</label>
                     <input type="password" class="form-control" id="new_password1" name="new_password1">
+                    <?= form_error('new_password1', '<small class="text-danger pl-3">', '</small>'); ?>
                 </div>
                 <div class="form-group">
                     <label for="new_password2">Repeat Password</label>
