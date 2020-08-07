9c2bebd3ac374fef4dca7ab95512911a07e623d8
adprm <aditiyaprmn00@gmail.com>
2020-08-07 09:17:37 +0700

    create input current password

--- application/views/user/changepassword.php
+++ application/views/user/changepassword.php
@@ -7,7 +7,10 @@
     <div class="row">
         <div class="col-lg-6">
             <form action="<?= base_url('user/changepassword'); ?>" method="post">
-                
+                <div class="form-group">
+                    <label for="current_password">Current Password</label>
+                    <input type="password" class="form-control" id="current_password" name="current_password">
+                </div>
             </form>
         </div>
     </div>
