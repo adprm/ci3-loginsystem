df3aa1220fc518d960b28d2db08b22519b352676
adprm <aditiyaprmn00@gmail.com>
2020-08-07 09:20:11 +0700

    form group change password

--- application/views/user/changepassword.php
+++ application/views/user/changepassword.php
@@ -11,6 +11,17 @@
                     <label for="current_password">Current Password</label>
                     <input type="password" class="form-control" id="current_password" name="current_password">
                 </div>
+                <div class="form-group">
+                    <label for="new_password1">New Password</label>
+                    <input type="password" class="form-control" id="new_password1" name="new_password1">
+                </div>
+                <div class="form-group">
+                    <label for="new_password2">Repeat Password</label>
+                    <input type="password" class="form-control" id="new_password2" name="new_password2">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Change Password</button>
+                </div>
             </form>
         </div>
     </div>
