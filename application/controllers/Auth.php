1fcf6d075f489af71fdf1875d1a29c83e86f82d3
adprm <aditiyaprmn00@gmail.com>
2020-08-11 07:43:07 +0700

    isactive false

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -95,7 +95,7 @@ class Auth extends CI_Controller {
                 'image' => 'default.jpg',
                 'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
                 'role_id' => 2,
-                'is_active' => 1,
+                'is_active' => 0,
                 'date_created' => time()
             ];
 
