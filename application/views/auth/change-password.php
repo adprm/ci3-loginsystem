540f6fb00756a617a85739410c46ca2d3c49bbc9
adprm <aditiyaprmn00@gmail.com>
2020-08-12 11:14:52 +0700

    make view change password

--- application/views/auth/change-password.php
+++ application/views/auth/change-password.php
@@ -16,20 +16,19 @@
                     <h5 class="mb-4"><?= $this->session->userdata('reset_email'); ?></h5>
                   </div>
                   <?= $this->session->flashdata('message'); ?>
-                  <form class="user" method="post" action="<?= base_url('auth/forgotpassword'); ?>">
+                  <form class="user" method="post" action="<?= base_url('auth/changepassword'); ?>">
                     <div class="form-group">
-                      <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..."
-                      value="<?= set_value('email'); ?>">
-                      <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+                      <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Enter new password">
+                      <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Repeat password">
+                      <?= form_error('password2', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <button type="submit" class="btn btn-primary btn-user btn-block">
-                      Reset Password
+                      Change Password
                     </button>
                   </form>
-                  <hr>
-                  <div class="text-center">
-                    <a class="small" href="<?= base_url('auth') ?>">Back to login</a>
-                  </div>
                 </div>
               </div>
             </div>
