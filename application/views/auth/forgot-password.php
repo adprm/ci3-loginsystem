6a1656e1e4e83395aef65ffcfaa33c2646f5ab96
adprm <aditiyaprmn00@gmail.com>
2020-08-12 10:32:34 +0700

    modified view forgot password

--- application/views/auth/forgot-password.php
+++ application/views/auth/forgot-password.php
@@ -12,29 +12,22 @@
               <div class="col-lg">
                 <div class="p-5">
                   <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Forgot your password ?</h1>
                   </div>
                   <?= $this->session->flashdata('message'); ?>
-                  <form class="user" method="post" action="<?= base_url('auth'); ?>">
+                  <form class="user" method="post" action="<?= base_url('auth/forgotpassword'); ?>">
                     <div class="form-group">
                       <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..."
                       value="<?= set_value('email'); ?>">
                       <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
-                    <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
-                      <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
-                    </div>
                     <button type="submit" class="btn btn-primary btn-user btn-block">
-                      Login
+                      Reset Password
                     </button>
                   </form>
                   <hr>
                   <div class="text-center">
-                    <a class="small" href="<?= base_url('auth/forgotpassword') ?>">Forgot Password?</a>
-                  </div>
-                  <div class="text-center">
-                    <a class="small" href="<?= base_url('auth/registration') ?>">Create an Account!</a>
+                    <a class="small" href="<?= base_url('auth') ?>">Back to login</a>
                   </div>
                 </div>
               </div>
